bfb3e4791d02f3e00cb4c2f0dc31abc496997787
Rafael Dias <rdmeneze@gmail.com>
2014-10-07 07:33:09 -0300

    - criacao do arquivo assert.c

--- header/defs.h
+++ header/defs.h
@@ -7,6 +7,8 @@
 #ifndef DEFS_H
 #define DEFS_H
 
+#include <stdint.h>
+
 typedef unsigned char BYTE;
 typedef unsigned short int WORD;
 typedef unsigned int  DWORD;
--- header/processor.h
+++ header/processor.h
@@ -1,7 +1,7 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
-#include <stdint.h>
+
 #include <system_stm32f4xx.h>
 #include "stm32f4xx_hal.h"
 
