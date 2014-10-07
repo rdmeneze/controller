c515b27f389d928c07a0ffb7ce063e0a820cbecb
Rafael Dias <rdmeneze@gmail.com>
2014-10-06 22:23:46 -0300

    - adicao da pasta header

---
+++ header/defs.h
@@ -0,0 +1,14 @@
+/**
+ *  @file   defs.h
+ *  @brief  type definitions
+ *  @author Rafael Dias <rafael.dias@silicaconsulting.com>
+ */
+
+#ifndef DEFS_H
+#define DEFS_H
+
+typedef unsigned char BYTE;
+typedef unsigned short int WORD;
+typedef unsigned int  DWORD;
+
+#endif
---
+++ header/processor.h
@@ -0,0 +1,8 @@
+#ifndef PROCESSOR_H
+#define PROCESSOR_H
+
+#include <stdint.h>
+#include <system_stm32f4xx.h>
+#include "stm32f4xx_hal.h"
+
+#endif
